bfb688d2f95fc3d6fa86155398db44de420157e7
Martin Keller <martin.keller@taketool.de>
2024-03-20 19:42:21 +0100

    rename to powermailextender and optimize code

--- Classes/Finisher/TonlineFinisher.php
+++ Classes/Finisher/TonlineFinisher.php
@@ -1,7 +1,6 @@
 <?php
 namespace Taketool\Powermailextender\Finisher;
 
-use In2code\Powermail\Domain\Model\Mail;
 use In2code\Powermail\Utility\MailUtility;
 use In2code\Powermail\Finisher\AbstractFinisher;
 
@@ -12,44 +11,6 @@ use In2code\Powermail\Finisher\AbstractFinisher;
  */
 class TonlineFinisher extends AbstractFinisher
 {
-
-    /**
-     * @var Mail
-     */
-    protected $mail;
-
-    /**
-     * @var array
-     */
-    protected $configuration;
-
-    /**
-     * @var array
-     */
-    protected $settings;
-
-    /* *
-     * Will be called always at first
-     *
-     * @return void
-     */
-    /*
-    public function initializeFinisher()
-    {
-    }
-    */
-
-    /* *
-     * Will be called before myFinisher()
-     *
-     * @return void
-     */
-    /*
-    public function initializeMyFinisher()
-    {
-    }
-    */
-
     /**
      * MyFinisher
      *
@@ -57,45 +18,19 @@ class TonlineFinisher extends AbstractFinisher
      */
     public function TonlineFinisher()
     {
-        // get value from configuration
-        //$foo = $this->configuration['foo'];
-        //debug($this->configuration, 'Powermail T-OnlineFinisher: $this->configuration');
-
-        // get data from mail
-        /*
-        debug([
-            'uidMail' => $this->getMail()->getUid(),
-            'pidStore' => $this->getMail()->getPid(),
-            'senderMail' => $this->getMail()->getSenderMail(), // mkeller.da@t-online.de
-            'senderName' => $this->getMail()->getSenderName(),
-            'receiverMail' => $this->getMail()->getReceiverMail(),
-            'subject' => $this->getMail()->getSubject(),
-            'body' => trim($this->getMail()->getBody()),
-
-        ], 'Powermail T-OnlineFinisher: $this->mail Data');
-        */
-        //debug($this->mail, 'Powermail T-OnlineFinisher: $this->mail');
-
-        // do some more magic ... t-online.de
         $senderMail = $this->getMail()->getSenderMail();
         if (strpos($senderMail, '@t-online.de') >0)
         {
             // send mail to receiver failed before and will be done here again
-            $mailSuccess = MailUtility::sendPlainMail(
-                $receiverMail = $this->getMail()->getReceiverMail(),
-                $senderMail = $this->getMail()->getReceiverMail(),  // yes that's right! Receiver gets mail only once...
-                $subject = $this->getMail()->getSubject(),
-                $body = "Sie haben eine neuen Eintrag von einer T-Online Adresse \n"
+            MailUtility::sendPlainMail(
+                $this->getMail()->getReceiverMail(),
+                $this->getMail()->getReceiverMail(),
+                $this->getMail()->getSubject(),
+                "Sie haben eine neuen Eintrag von einer T-Online Adresse \n"
                     .$this->getMail()->getSenderName()
                     .'<'.$this->getMail()->getSenderMail().">\n"
-                    ."in Ihrem Powermail Ordner mit der SeitenID=".$this->getMail()->getPid().'.'
+                    .'in Ihrem Powermail Ordner.'
             );
-            if ($mailSuccess)
-            {
-                //debug($mailSuccess,'Mail successful sent');
-            } else {
-                //debug($mailSuccess,'Mail not successful sent');
-            }
         }
     }
 }
